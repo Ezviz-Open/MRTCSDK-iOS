4c73deadae9506bc0bda3456548701e3a0115550
1020481644@qq.com <wangjinlong23@wangjinlong23deMac-mini.local>
2026-01-15 10:01:27 +0800

    ‘删除无用参数’

--- MRTCSDK/MRTCSDK.framework/Headers/MRTCSDKJoinRoomParam.h
+++ MRTCSDK/MRTCSDK.framework/Headers/MRTCSDKJoinRoomParam.h
@@ -15,18 +15,10 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) VCSMeetingParam *vcsMeetingParam;
 ///是否开启NoPickAudio模式(视频默认选择4方，音频选择所有发言者)
 @property (nonatomic, assign) BOOL isNoPickAudio;
-///自研 ERTC 加入房间的鉴权 token
-@property (nonatomic, copy) NSString *ertcToken;
 ///会议房间号
 @property (nonatomic, copy) NSString *roomNo;
 ///用户userID
 @property (nonatomic, copy) NSString *userID;
-
-//房间内请求地址
-@property(nonatomic, strong) NSString *apiUrl;
-
-//appID 外部传入
-@property(nonatomic, strong) NSString *appID;
 
 //是否是研讨会嘉宾
 @property (nonatomic, assign) BOOL isSeminarGuest;
